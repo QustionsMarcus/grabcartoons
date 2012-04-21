e578204f9070d20c360fb5de7d1fb46128db4b0b
QustionsMarcus <tbuell@gmail.com>
2012-04-21 16:43:58 -0300

    Updated modules/ffn.pl to reflect changes in the location of comic's home page and the regex for the comic.

--- modules/ffn.pl
+++ modules/ffn.pl
@@ -1,7 +1,6 @@
 $COMIC{ffn} = {
     Title => 'Full Frontal Nerdity',
-    Page => 'http://nodwick.humor.gamespy.com/ffn',
-    Regex => qr(img src="http\://nodwick\.humor\.gamespy\.com/ffn/(strips/.*\.jpg)"),
-    Prepend => '{Page}/',
+    Page => 'http://ffn.nodwick.com',
+    Regex => qr!img src="(http://ffn.malletspace.net/ffnstrips/201\d\-\d\d\-\d\d.png)"!,
     NoShowTitle => 1,
 };
