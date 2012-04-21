54e608e9c9def840b63b59ce4569433c22008c3e
QustionsMarcus <tbuell@gmail.com>
2012-04-21 16:51:08 -0300

    Updated moules/nodwick.com to reflect changes in the comic's home page and the HTML for the comic itself.

--- modules/nodwick.pl
+++ modules/nodwick.pl
@@ -1,6 +1,6 @@
 $COMIC{nodwick} = {
     Title => 'Nodwick',
-    Page => 'http://nodwick.humor.gamespy.com/gamespyarchive',
-    Regex => qr(img src="http\://nodwick\.humor\.gamespy\.com/gamespyarchive/(strips/.*\.jpg)"),
+    Page => 'http://comic.nodwick.com',
+    Regex => qr!img src="http://comic.nodwick.com/(nodwickstrips/201\d\-\d\d\-\d\d.png)"!,
     Prepend => '{Page}/',
 };
